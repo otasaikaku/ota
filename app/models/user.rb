2dcc394636c2f808406d9032aa7dfebdb5adf388
Saikaku Ota <satokeigo0316@gmail.com>
2021-09-20 13:54:28 +0900

    バリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_tweets, through: :likes, source: :tweet
   has_many :comments, dependent: :destroy
+  validates :car_lovers, presence: true
 
   def already_liked?(tweet)
     self.likes.exists?(tweet_id: tweet.id)
